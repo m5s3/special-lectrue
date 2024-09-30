b5c2ae10801b53b1d6aafac20d466f6ab15a30e0
m5s3 <devmyoungsoo2@gmail.com>
2024-09-30 16:52:58 +0900

    init

---
+++ src/main/java/com/speciallecture/SpecialLectureApplication.java
@@ -0,0 +1,13 @@
+package com.speciallecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpecialLectureApplication.class, args);
+    }
+
+}
